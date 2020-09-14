4853700f13a3d90d40cc4460b5d51b0bd6c6a612
HSabbir <43398598+HSabbir@users.noreply.github.com>
2020-09-14 13:40:17 -0700

    project work done

--- project/models.py
+++ project/models.py
@@ -1,11 +1,27 @@
 from django.conf import settings
 from django.db import models
 from workingSkills.models import Domain,Environment,ToolsAndTechnology
+from account.models import Account
+
+
 
 class Project(models.Model):
-    profile_of = models.ManyToManyField(settings.AUTH_USER_MODEL, null=False)
+
     project_title = models.CharField(max_length=100)
     domain = models.ManyToManyField(Domain)
     environment = models.ManyToManyField(Environment)
     tools_and_technology = models.ManyToManyField(ToolsAndTechnology)
+    profile = models.ManyToManyField(Account)
+
+
+class CreateProject(models.Model):
+    project_title = models.CharField(max_length=100)
+    domain = models.ManyToManyField(Domain)
+    environment = models.ManyToManyField(Environment)
+    tools_and_technology = models.ManyToManyField(ToolsAndTechnology)
+    profile = models.ManyToManyField(Account)
+    project_photo = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.project_title
 
--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from project.views import viewProject
 
 urlpatterns = [
     path('', views.userProfileview, name='createprofile'),
+
 
 ]
